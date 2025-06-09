72554e20391d7f05e8863f6b166bca1f15b55c2c
Febriyan Ahza <69071485+novaru@users.noreply.github.com>
2025-06-09 08:31:30 +0700

    feat: migrate spago

--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -2,4 +2,19 @@ module Test.MySolutions where
 
 import Prelude
 
--- Note to reader: Add your solutions to this file
+import Data.AddressBook (AddressBook, Entry, findEntry)
+import Data.List (filter, head, nubByEq)
+import Data.Maybe (Maybe, isJust)
+
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street =
+  head <<< filter (\entry -> entry.address.street == street)
+
+isInBook :: String -> String -> AddressBook -> Boolean
+isInBook fname lname book = isJust $ findEntry fname lname book
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates = nubByEq duplicatedEntries
+  where
+  duplicatedEntries :: Entry -> Entry -> Boolean
+  duplicatedEntries e1 e2 = e1.firstName == e2.firstName && e1.lastName == e2.lastName
